1ba9a91935d182af1b1ec6b28a6ee49b448384e2
Dhanush Eashwar <dhanush.eashwar@gmail.com>
2025-10-07 12:21:42 -0700

    Initial Commit

---
+++ URAP Polar H10 V1/URAP_Polar_H10_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  URAP_Polar_H10_V1App.swift
+//  URAP Polar H10 V1
+//
+//  Created by Dhanush Eashwar on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct URAP_Polar_H10_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
